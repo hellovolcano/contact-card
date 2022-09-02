6f290a319e132b885cdfab884c7c9c7ff679ee1c
hellovolcano <valerie.gleason@gmail.com>
2022-09-02 15:33:28 -0500

    adding get/post requests

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,3 +15,35 @@ export const initDb = async () => {
         }   
     })
 }
+
+export const getDb = async() => {
+    const db = await openDB('contact_db')
+    const tx = db.transaction('contacts','readonly')
+    const store = tx.objectStore('contacts')
+    const request = store.getAll()
+    
+    const result = await request
+    console.log('result.value', result)
+
+    return result
+}
+
+export const createStuff = async(name, email, phone, profile) => {
+
+    // Create a connection to the database and specify the version we want to use.
+    const db = await openDB('contact_db',1)
+
+    // Create a new transaction and specify the store and data privileges.
+    const tx = db.transaction('contacts', 'readwrite')
+
+    // Open up the desired object store.
+    const store = tx.objectStore('contacts')
+
+    // Use the .add() method on the store and pass in the content.
+    const request =  store.add({ name: name, email: email, phone: phone, profile: profile })
+
+    // Get confirmation of the request
+    const result = await request
+
+    console.log('🚀 - data saved to the database', result);
+}
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,8 @@
-import "./form"
-import "./submit"
+import { clearForm, toggleForm } from "./form"
+// import "./submit"
 import "./database"
+import { createStuff, getDb, initDb } from "./database"
+import { fetchCards } from "./cards"
 
 // Import CSS
 import "../css/index.css"
@@ -13,11 +15,48 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Logo from "../images/cageBunny.jpeg"
 import Bear from "../images/bear.png"
 import Dog from "../images/dog.png"
-import { initDb } from "./database"
+
 
 window.addEventListener('load', function() {
     initDb()
+    fetchCards()
     document.getElementById('logo').src = Logo
     document.getElementById('bearThumbnail').src = Bear
     document.getElementById('dogThumbnail').src = Dog
 })
+
+  // Form functionality
+  const form = document.getElementById("formToggle");
+  const newContactButton = document.getElementById("new-contact");
+  let submitBtnToUpdate = false;
+  let profileId;
+  
+  newContactButton.addEventListener('click', event => {
+    toggleForm()
+   })
+  
+  form.addEventListener('submit', event => {
+    // Handle data
+    event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+  
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    createStuff(name, email, phone, profile);
+  } else {
+  
+    fetchCards();
+      // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+  
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
+  });
